01c7b805b7fe22dab13ff54d15de0500f6e3fe00
Rutherfordum <amaterasu.dzen@gmail.com>
2022-08-31 17:50:20 +0300

    Create bezier object

---
+++ Assets/Bezier/Scripts/Bezier.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Bezier
+{
+    //P = ((1−t)^3)*P0 + 3*((1−t)^2)*t*P1 +3*(1−t)*(t^2)*P2 + (t^3)*P3
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float OneMinesT = 1f - t;
+        return
+            Mathf.Pow(OneMinesT, 3) * p0 +
+            3 * Mathf.Pow(OneMinesT, 2) * t * p1 +
+            3 * OneMinesT * Mathf.Pow(t, 2) * p2 +
+            Mathf.Pow(t, 3) * p3;
+    }
+
+    // производная от GetPoint, чтобы получить направление на кривой 
+    public static Vector3 GetDirection(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float OneMinesT = 1f - t;
+        return
+            3f * Mathf.Pow(OneMinesT, 2) * (p1 - p0) +
+            6f * OneMinesT * t * (p2 - p1) +
+            3f * Mathf.Pow(t, 2) * (p3 - p2);
+
+    }
+}
---
+++ Assets/Bezier/Scripts/BezierFourPoint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteAlways]
+public class BezierFourPoint : MonoBehaviour
+{
+    public Transform P0 => p0;
+    public Transform P1 => p1;
+    public Transform P2 => p2;
+    public Transform P3 => p3;
+
+    public float T
+    {
+        get => t;
+        set { t = (!(value < 0 || value > 1)) ?
+            value : throw new Exception("T cannot be less than zero or greater than one"); }
+    }
+
+    [SerializeField] private Transform p0;
+    [SerializeField] private Transform p1;
+    [SerializeField] private Transform p2;
+    [SerializeField] private Transform p3;
+    
+    [Range(0,1)]
+    [SerializeField] private float t = 0;
+
+    private void Update()
+    {
+        transform.position = Bezier.GetPoint(P0.position, P1.position, P2.position, P3.position, T);
+        transform.rotation = Quaternion.LookRotation(Bezier.GetPoint(P0.position, P1.position, P2.position, P3.position, T));
+    }
+
+    private void OnDrawGizmos()
+    {
+        int segmentsNumber = 20;
+        Vector3 prevPoint = P0.position;
+
+        for (int i = 0; i < segmentsNumber+1; i++)
+        {
+            float parameter = (float) i / segmentsNumber;
+            Vector3 point = Bezier.GetPoint(P0.position, P1.position, P2.position, P3.position, parameter);
+            Gizmos.DrawLine(prevPoint,point);
+            prevPoint = point;
+        }
+    }
+}
